c58295e015fc7a6a78f8a71df82db71d151de6a4
popschool <popschool@debian>
2017-09-27 16:41:49 +0200

    add mini-jeux 2

--- orienté_objet/heritage/perso.php
+++ orienté_objet/heritage/perso.php
@@ -1,200 +1,333 @@
 <?php
 
-abstract class Personnage {
+abstract class Personnage
+
+{
+
   protected $atout,
-  $degats,
-  $id,
-  $nom,
-  $timeEndormi,
-  $type;
 
-  const CEST_MOI = 1;
-  const PERSONNAGE_TUE = 2;
-  const PERSONANGE_FRAPPE = 3;
-  const PERSONNAGE_ENSORCELE = 4;
-  const PAS_DE_MAGIE = 5;
-  const PERSONNAGE_ENDORMI = 6;
+            $degats,
 
-  public function __construct(array $donnees) {
-    $this->hydrate($données);
+            $id,
+
+            $nom,
+
+            $timeEndormi,
+
+            $type;
+
+
+
+  const CEST_MOI = 1; // Constante renvoyée par la méthode `frapper` si on se frappe soit-même.
+
+  const PERSONNAGE_TUE = 2; // Constante renvoyée par la méthode `frapper` si on a tué le personnage en le frappant.
+
+  const PERSONNAGE_FRAPPE = 3; // Constante renvoyée par la méthode `frapper` si on a bien frappé le personnage.
+
+  const PERSONNAGE_ENSORCELE = 4; // Constante renvoyée par la méthode `lancerUnSort` (voir classe Magicien) si on a bien ensorcelé un personnage.
+
+  const PAS_DE_MAGIE = 5; // Constante renvoyée par la méthode `lancerUnSort` (voir classe Magicien) si on veut jeter un sort alors que la magie du magicien est à 0.
+
+  const PERSO_ENDORMI = 6; // Constante renvoyée par la méthode `frapper` si le personnage qui veut frapper est endormi.
+
+
+
+  public function __construct(array $donnees)
+
+  {
+
+    $this->hydrate($donnees);
+
     $this->type = strtolower(static::class);
+
   }
 
-  public function estEndormi() {
+
+
+  public function estEndormi()
+
+  {
+
     return $this->timeEndormi > time();
+
   }
 
-  public function frapper(Personnage $perso) {
-    if ($perso->id == $this->id) {
+
+
+  public function frapper(Personnage $perso)
+
+  {
+
+    if ($perso->id == $this->id)
+
+    {
+
       return self::CEST_MOI;
+
     }
-    if ($this->estEndormi()) {
-      return self::PERSONNAGE_ENDORMI;
+
+
+
+    if ($this->estEndormi())
+
+    {
+
+      return self::PERSO_ENDORMI;
+
     }
+
+
+
+    // On indique au personnage qu'il doit recevoir des dégâts.
+
+    // Puis on retourne la valeur renvoyée par la méthode : self::PERSONNAGE_TUE ou self::PERSONNAGE_FRAPPE.
+
     return $perso->recevoirDegats();
+
   }
 
-  public function hydrate(array $donnees) {
-    foreach ($donnees as $key => $value) {
+
+
+  public function hydrate(array $donnees)
+
+  {
+
+    foreach ($donnees as $key => $value)
+
+    {
+
       $method = 'set'.ucfirst($key);
-      if (method_exists($this, $method)) {
+
+
+
+      if (method_exists($this, $method))
+
+      {
+
         $this->$method($value);
+
       }
+
     }
+
   }
 
-  public function nomValide() {
+
+
+  public function nomValide()
+
+  {
+
     return !empty($this->nom);
+
   }
 
-  public function recevoirDegats() {
+
+
+  public function recevoirDegats()
+
+  {
+
     $this->degats += 5;
-    if ($this->degats >= 100) {
+
+
+
+    // Si on a 100 de dégâts ou plus, on supprime le personnage de la BDD.
+
+    if ($this->degats >= 100)
+
+    {
+
       return self::PERSONNAGE_TUE;
+
     }
-    return self::PERSONANGE_FRAPPE;
+
+
+
+    // Sinon, on se contente de mettre à jour les dégâts du personnage.
+
+    return self::PERSONNAGE_FRAPPE;
+
   }
 
-  public function reveil() {
+
+
+  public function reveil()
+
+  {
+
     $secondes = $this->timeEndormi;
+
     $secondes -= time();
 
+
+
     $heures = floor($secondes / 3600);
+
     $secondes -= $heures * 3600;
+
     $minutes = floor($secondes / 60);
+
     $secondes -= $minutes * 60;
 
-    $heures .= $heures <= 1 ? ' heure ' : ' heures ';
-    $minutes .= $minutes <= 1 ? ' minute ' : ' minutes ';
-    $secondes .= $secondes <= 1 ? ' seconde ' : ' secondes ';
 
-    return $heures . ' , ' . $minutes . ' et ' . $secondes;
+
+    $heures .= $heures <= 1 ? ' heure' : ' heures';
+
+    $minutes .= $minutes <= 1 ? ' minute' : ' minutes';
+
+    $secondes .= $secondes <= 1 ? ' seconde' : ' secondes';
+
+
+
+    return $heures . ', ' . $minutes . ' et ' . $secondes;
+
   }
 
-  public function atout() {
+
+
+  public function atout()
+
+  {
+
     return $this->atout;
+
   }
 
-  public function degats() {
+
+
+  public function degats()
+
+  {
+
     return $this->degats;
+
   }
 
-  public function id() {
+
+
+  public function id()
+
+  {
+
     return $this->id;
+
   }
 
-  public function nom() {
+
+
+  public function nom()
+
+  {
+
     return $this->nom;
+
   }
 
-  public function timeEndormi() {
+
+
+  public function timeEndormi()
+
+  {
+
     return $this->timeEndormi;
+
   }
 
-  public function type() {
+
+
+  public function type()
+
+  {
+
     return $this->type;
+
   }
 
-  public function setAtout($atout) {
+
+
+  public function setAtout($atout)
+
+  {
+
     $atout = (int) $atout;
 
-    if ($atout >= 0 && $atout <= 100) {
+
+
+    if ($atout >= 0 && $atout <= 100)
+
+    {
+
       $this->atout = $atout;
+
     }
+
   }
 
-  public function setDegats($degats) {
+
+
+  public function setDegats($degats)
+
+  {
+
     $degats = (int) $degats;
 
-    if ($degats >= 0 && $degats <= 100) {
+
+
+    if ($degats >= 0 && $degats <= 100)
+
+    {
+
       $this->degats = $degats;
+
     }
+
   }
 
-  public function setId($id) {
+
+
+  public function setId($id)
+
+  {
+
     $id = (int) $id;
 
-    if ($id >= 0) {
+
+
+    if ($id > 0)
+
+    {
+
       $this->id = $id;
+
     }
+
   }
 
-  public function setNom($nom) {
-    if (is_string($nom)) {
+
+
+  public function setNom($nom)
+
+  {
+
+    if (is_string($nom))
+
+    {
+
       $this->nom = $nom;
+
     }
+
   }
 
-  public function setTimeEndormi($time) {
+
+
+  public function setTimeEndormi($time)
+
+  {
+
     $this->timeEndormi = (int) $time;
+
   }
 
-}
-
-class Magicien extends Personnage {
-
-  public function lancerUnSort(Personnage $perso){
-    if ($this->degats >= 0 && $this->degats <= 25) {
-      $this->atout = 4;
-    }
-    elseif ($this->degats > 25 && $this->degats <= 50) {
-      $this->atout = 3;
-    }
-    elseif ($this->degats > 50 && $this->degats <= 75) {
-      $this->atout = 2;
-    }
-    elseif ($this->degats > 75 && $this->degast <= 90) {
-      $this->atout = 1;
-    }
-    else {
-      $this->atout = 0;
-    }
-
-    if ($perso->id == $this->id) {
-      return self::CEST_MOI;
-    }
-
-    if ($this->atout == 0) {
-      return self::PAS_DE_MAGIE;
-    }
-
-    if ($this->estEndormi()) {
-      return self::PERSONNAGE_ENDORMI;
-    }
-    $perso->timeEndormi = time() + ($this->atout * 6) * 3600;
-    return self::PERSONNAGE_ENSORCELE;
-    
-  }
-}
-
-class Guerrier extends Personnage {
-  public function recevoirDegats() {
-    if ($this->degats >= 0 && $this->degats <= 25) {
-      $this->atout = 4;
-    }
-    elseif ($this->degats > 25 && $this->degats <= 50) {
-      $this->atout = 3;
-    }
-    elseif ($this->degats > 50 && $this->degats <= 75) {
-      $this->atout = 2;
-    }
-    elseif ($this->degats > 75 && $this->degast <= 90) {
-      $this->atout = 1;
-    }
-    else {
-      $this->atout = 0;
-    }
-    $this->degats += 5 - $this->atout;
-
-    if ($perso->id == $this->id) {
-      return self::CEST_MOI;
-    }
-
-    if ($this->degats >= 100) {
-      return self::PERSONNAGE_TUE;
-    }
-    else {
-      return self::PERSONANGE_FRAPPE;
-    }
-  }
 }
